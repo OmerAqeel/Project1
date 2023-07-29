e03d700917826bed4f86b6ced40ba0b2c069f91b
Omer Aqeel <omrraqeel@gmail.com>
2023-07-29 19:53:52 +0100

    Added the navbar to the web app

--- client-side/src/components/homePage.jsx
+++ client-side/src/components/homePage.jsx
@@ -4,7 +4,10 @@ import { Link, NavLink, Bars } from "react-router-dom";
 const HomePage = () => {
   return (
     <div>
+      <div>
+       
       <h4>HomePage</h4>
+      </div>
     </div>
   );
 };
--- client-side/src/components/navbar.jsx
+++ client-side/src/components/navbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Link, NavLink, Bars } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./navbarStyle.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h4>Navbar</h4>
-    </div>
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/" className="nav-links">Home</Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">Categories</Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-links">Sell Books</Link>
+        </li>
+      </ul>
+      <Link to="/" className="loginBtn">
+        <button>Login</button>
+      </Link>
+    </nav>
   );
 };
 
