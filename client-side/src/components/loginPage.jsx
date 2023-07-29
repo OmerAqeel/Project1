06f2cd08254abe1cfe239256721ac8eff146dbeb
Omer Aqeel <omrraqeel@gmail.com>
2023-07-29 23:12:03 +0100

    Logo added to the navbar

--- client-side/src/components/loginPage.jsx
+++ client-side/src/components/loginPage.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import { Link, NavLink, Bars } from "react-router-dom";
 
 const LoginPage = () => {
+
+    
   return (
-    <div>
-      <div>
-       
-      <h4>LoginPage</h4>
+    <div className="sign-in-container">
+    <h2>Sign In</h2>
+    <form >
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+        />
       </div>
-    </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          required
+        />
+      </div>
+      <button type="submit">Sign In</button>
+    </form>
+  </div>
   );
 };
 
