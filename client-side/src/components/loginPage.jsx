f98df17bf596a316f96531799409daa14272af26
Omer Aqeel <omrraqeel@gmail.com>
2023-08-12 21:22:24 +0100

    login updated

--- client-side/src/components/loginPage.jsx
+++ client-side/src/components/loginPage.jsx
@@ -1,8 +1,50 @@
-import React from "react";
-import { Link, NavLink, Bars } from "react-router-dom";
-import './loginStyle.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './loginStyle.css';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (username.trim() === "") {
+      setUsernameError("Username is required");
+      isValid = false;
+    } else {
+      setUsernameError("");
+    }
+
+    if (password.trim() === "") {
+      setPasswordError("Password is required");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (validateForm()) {
+      // Form is valid, you can proceed with submission or further processing
+      console.log("Form submitted successfully");
+    }
+  };
+
   return (
     <div className="sign-in-page">
       <div className="info-pane">
@@ -10,25 +52,41 @@ const LoginPage = () => {
         <br />
         <br />
         <div className="info-content">
-        <h2 className="content Title">Welcome aboard my zozo!</h2>
-        <p className="content Para">Hero na ban sheeday </p>
+          <h2 className="content Title">Welcome aboard my zozo!</h2>
+          <p className="content Para">Hero na ban sheeday </p>
         </div>
       </div>
       <div className="sign-in-pane">
         <h2>Sign In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-container">
             <label htmlFor="username">
               <i className="fas fa-envelope"></i>
             </label>
-            <input type="text" id="username" name="username" required />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={handleUsernameChange}
+              required
+            />
+            <p className="error">{usernameError}</p>
           </div>
           <br />
           <div className="input-container"> 
             <label htmlFor="password">
               <i className="fas fa-lock"></i>
             </label>
-            <input type="password" id="password" name="password" required />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+            <p className="error">{passwordError}</p>
           </div>
           <br />
           <div>
